8e291424e5e16c2e91f75db41b6dfc6e2fb0f9a2
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-11 11:57:00 +0200

    Start working on graphing of time spent

---
+++ public/graph.js
@@ -0,0 +1,31 @@
+let graphContainer = gd("graph-container");
+
+function countHeartbeatTimes(heartbeats,projects) {
+    let datapoints = [];
+    let dates = {};
+    let registeredTimes = {};
+    let minutesTrack = 2;
+    let firstTime = undefined;
+    for (let heartbeat of heartbeats) {
+        if (!projects.includes(heartbeat.project)) {
+            // continue;
+        }
+        if (!firstTime || firstTime > heartbeat.time) {
+            firstTime = heartbeat.time;
+        }
+        let date = new Date(heartbeat.time*1000);
+        date.setMilliseconds(0);
+        date.setSeconds(0);
+        date.setMinutes(Math.floor(date.getMinutes()/minutesTrack)*minutesTrack);
+        if (!registeredTimes[date.valueOf()]) {
+            registeredTimes[date.valueOf()] = true;
+        } else {
+            continue;
+        }
+
+        date.setMinutes(0);
+        date.setHours(0);
+        dates[date.valueOf()] = (dates[date.valueOf()] || 0) + minutesTrack;
+    }
+}
+// countHeartbeatTimes(cached.heartbeats,[cached.heartbeats[0].project]);

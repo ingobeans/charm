009d058cd71a95ef69705358ccacdd980781b3ec
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-12 00:01:13 +0200

    Fix bugs with graph renderer on big projects

--- public/graph.js
+++ public/graph.js
@@ -2,15 +2,17 @@ let graphContainer = gd("graph-container");
 let graphCanvas = gd("graph-canvas");
 let ctx = graphCanvas.getContext("2d");
 
+let cachedDates = {};
 function countHeartbeatTimes(heartbeats, projects) {
     let dates = {};
     let registeredTimes = {};
     let minutesTrack = 2;
     let firstTime = undefined;
+    let firstTimeDate = undefined;
     let lastTime = 0;
     for (let heartbeat of heartbeats) {
         if (!projects.includes(heartbeat.project)) {
-            // continue;
+            continue;
         }
         let date = new Date(heartbeat.time * 1000);
         date.setMilliseconds(0);
@@ -24,17 +26,23 @@ function countHeartbeatTimes(heartbeats, projects) {
 
         date.setMinutes(0);
         date.setHours(0);
-        let dateValue = date.valueOf();
+        let dateValue = Math.floor(heartbeat.time / 60 / 60 / 24);
         if (!firstTime || firstTime > dateValue) {
             firstTime = dateValue;
+            firstTimeDate = heartbeat.time;
         }
         if (lastTime < dateValue) {
             lastTime = dateValue;
         }
         dates[dateValue] = (dates[dateValue] || 0) + minutesTrack;
     }
+    cachedDates = dates;
+    console.log("Finished parsing heartbeat data");
+    console.log(dates);
+    let dateAmt = 0;
     let highest = 0;
     for (let [k, v] of Object.entries(dates)) {
+        dateAmt++;
         if (v > highest) {
             highest = v;
         }
@@ -50,18 +58,20 @@ function countHeartbeatTimes(heartbeats, projects) {
     graphCanvas.height = canvasHeight;
 
     function getDateX(date) {
-        return date / 1000 / 60 / 60 / 24 * 100;
+        return date * 100;
     }
-
     function iterateDates(callback) {
         let index = 0;
-        let dateAmt = count(Object.keys(dates));
         let dateCount = 0;
         while (true) {
-            let date = firstTime + 1 * 24 * 60 * 60 * 1000 * index;
+            let date = firstTime + 1 * index;
             let value = dates[date];
             if (value != undefined) {
                 dateCount++;
+            }
+            if (date > lastTime) {
+                console.warn("Something went wrong loading dates. Soft failing");
+                break;
             }
 
             callback(date, value);
@@ -80,7 +90,6 @@ function countHeartbeatTimes(heartbeats, projects) {
     iterateDates((date, value) => {
         let x = getDateX(date - firstTime) * horizontalScale + xOffset;
         let y = (value || 0) * verticalScale;
-        console.log(x, y);
         ctx.lineTo(x, canvasHeight - y - yOffset);
     });
     ctx.strokeStyle = "red";
@@ -90,18 +99,16 @@ function countHeartbeatTimes(heartbeats, projects) {
     ctx.fillStyle = "gray";
     iterateDates((date, value) => {
         let x = getDateX(date - firstTime) * horizontalScale + xOffset;
-        let dateObj = new Date(date);
-        ctx.fillText(dateObj.getDay() + "/" + dateObj.getMonth(), x, canvasHeight);
+        let dateObj = new Date(date * 24 * 60 * 60 * 1000);
+        ctx.fillText(dateObj.getDate() + "/" + dateObj.getMonth(), x, canvasHeight);
     });
 
     let verticalUnitSteps = 5;
 
     let verticalUnitSize = highest / (verticalUnitSteps);
-    console.log(highest);
     for (let i = 0; i <= verticalUnitSteps; i++) {
         let v = verticalUnitSize * i;
-        console.log(v / 60);
         ctx.fillText((v / 60).toFixed(1), canvasWidth - 40, canvasHeight - v * verticalScale - yOffset + 9);
     }
 }
-countHeartbeatTimes(cached.heartbeats, [cached.heartbeats[0].project]);
+// countHeartbeatTimes(cached.heartbeats, [cached.heartbeats[0].project]);
